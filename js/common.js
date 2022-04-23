ca68b1fc9d29681a22816eb3ad860666254ca97d
iamning1993 <iamning1993@163.com>
2022-04-23 16:48:37 +0800

    网站修改提交

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 !(function () {
     var nav = {
-        "index": {
+        "": {
             id: 1,
             name: "首页",
             href: "./"
@@ -17,20 +17,29 @@
         },
         "contact": {
             id: 4,
-            name: "联系我们",
+            name: "人才招聘",
             href: "./contact.html"
         }
     };
 
+    var header = "陕西天效环境建设有限公司";
+    var footer = "陕西天效环境建设有限公司 Copyright © 2022 版权所有 陕ICP备2022004101号";
+
     function createNav () {
+        var urlName = window.location.pathname.replace(".html", "").replace("/", "");
         var html = '';
         for (var k in nav) {
             var item = nav[k];
-            html += '<li><a class="nav-a" data-id="' + item.id + '" href="' + item.href + '">' + item.name + '</a></li>';
+            if (urlName === k) {
+                html += '<li><a class="nav-a" style="border-bottom: 2px solid #00BFFF;font-weight: bolder;" data-id="' + item.id + '" href="' + item.href + '">' + item.name + '</a></li>';
+            } else {
+                html += '<li><a class="nav-a" data-id="' + item.id + '" href="' + item.href + '">' + item.name + '</a></li>'; 
+            }
         }
+        document.getElementById("header").innerText = header;
+        document.getElementById("footer").innerText = footer;
         document.getElementById("nav").innerHTML = html;
     }
-
 
     window.SXTSHJ = {
         createNav: createNav
